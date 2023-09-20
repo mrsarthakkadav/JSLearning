ddb730173da1a13a07c8104ad3fb1d013309e397
Sarthak Kadav <mrsarthakkadav@gmail.com>
2023-09-20 09:09:12 +0530

    Assignment on variable
    Assignment on variable

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+var firstName;
+firstName="Sarthak";
+console.log(firstName);
+firstName="mr.Sarthak";
+console.log(firstName);
+
+var lastName="Kadav";
+console.log(lastName);
+
+var height;
+console.log(height);
+height="6";
+console.log(height);
+height="6.2"
+console.log(height);
