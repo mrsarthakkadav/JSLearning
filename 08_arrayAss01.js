ec5e505595dfe5fc64fddbe995110916342ceb0b
mrsarthakkadav <mrsarthakkadav@gmail.com>
2023-10-05 22:19:18 +0530

    changes in code
    changes in code

---
+++ 08_arrayAss01.js
@@ -0,0 +1,13 @@
+let arrayNumbers = [22,11,44,55,77,33];
+let sum=0;
+console.log(`============ 1st program============`);
+for (let index = 0; index < arrayNumbers.length; index=index+2) 
+{
+    console.log(arrayNumbers[index]); 
+    
+    sum += arrayNumbers[index];
+}
+console.log(`============ 2nd program============`);
+
+
+console.log(`Sum of the element of array :- ${sum}`);
